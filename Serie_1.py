ed01bc6f40ec53122cfd1ce2691b8328aaadad13
Alan Gustavo <60892674+AlanGustav0@users.noreply.github.com>
2020-04-21 20:10:04 -0300

    Primeira série recursiva - 1

---
+++ Serie_1.py
@@ -0,0 +1,18 @@
+'''
+Este algoritimo faz a soma da seguinte sequência: 
+1 + 2 + 3 +...+ 100 
+
+'''
+
+n = 100
+
+def Soma(x):
+   
+  if (x ==1):
+    return x
+
+  else:
+
+    return x + (Soma(x-1))
+
+print(Soma(n))
