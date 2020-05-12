1baa31495ea2757aeb9dc1540c14c39d19ae5f1c
Alan Gustavo <60892674+AlanGustav0@users.noreply.github.com>
2020-05-11 21:18:06 -0300

    Update file

--- Fibonnacci/fibonacci.py
+++ Fibonnacci/fibonacci.py
@@ -1,28 +1,47 @@
+ok = False
+
+
+def validation(number):
+    global ok
+
+    if number <= 0:
+        print('Erro! Invalid number!')
+    else:
+        ok = True
+        return ok
+
+
+
 def Fibo(number):
 
     previous = 1
     current = 1
     auxiliary = 0
-
     count = 0
 
-    print('0 - 1 - 1')
+
 
     while count < number:
 
-        next = current + previous
+        
+        if count < 1:
+            print(count,'- 1 - ',count + 1, end=' ')
+        else:   
+            next = current + previous
+            print('- {}'.format(next), end=' ')
 
-        print('- {}'.format(next),end=' ')
-
-        previous = current
-        current = next
-
+            previous = current
+            current = next
         count += 1
 
 
-        
-        
-number = (int(input('Isert the number: ')))
+while ok == False:
+
+    try:
+        number = (int(input('Isert the number: ')))
+        validation(number)
+    except:
+        ValueError
+        print('Erro! Invalid value!')
 
 Fibo(number)
-
