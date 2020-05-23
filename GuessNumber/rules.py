5ebab35b9f881fd8ef57c418beb9fca594b3bcef
Alan Gustavo <60892674+AlanGustav0@users.noreply.github.com>
2020-05-23 20:17:17 -0300

    Update files

--- GuessNumber/rules.py
+++ GuessNumber/rules.py
@@ -1,22 +1,24 @@
 import start
 
 
-def validate(valid, verify):
-    if valid.isalpha() and valid == "y":
-        start.start(verify)
-    elif valid.isalpha() and valid == "n":
+def validate(valid, first_time):
+    if valid == "y":
+        start.start(first_time)
+
+    elif valid == "n":
         print('Good Bye!')
         exit()
     else:
         print('Invalid option')
 
 
-def remake(number, pc_number, verify):
+def remake(number, pc_number, first_time):
     done = False
     if number == pc_number:
         print('You win! It was this number! \n')
     else:
         print('Oops, you lose! The number was {} try again!\n'.format(pc_number))
+
     while not done:
         valid = str(input('Do you want try again? Insert (Y) for Yes and ?(N) for No: ')).lower()
-        validate(valid, verify)
+        validate(valid, first_time)
--- GuessNumber/start.py
+++ GuessNumber/start.py
@@ -1,12 +1,24 @@
 import generate
-from random import randint
+import levels
 
 
 def start(first_time):
+    choose = False
     if first_time:
         name = str(input('Insert your name: ')).title()
         first_time = False
-        print('Hello {}, are you ready? \n'.format(name))
-    pc_number = randint(0, 5)
-    generate.thinking()
-    generate.choice_number(pc_number, first_time)
+        print('Hello {}, ready to play? \n'.format(name))
+        print('The objective of this game is to guess the number I thought..\n')
+
+    while not choose:
+
+        level = int(input('**Choose the difficult level** \n '
+                          '(1) Easy -  (2)Medium - (3)Hard: '))
+        
+        if level == 1 or level == 2 or level == 3:
+            backupLevel = level
+            levels.Level(level)
+            generate.thinking()
+            generate.choice_number(level, first_time, backupLevel)
+        else:
+            print('Invalid value! Try again!\n')
